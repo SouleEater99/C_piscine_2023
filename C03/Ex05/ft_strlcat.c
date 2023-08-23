fb5da44496e2e87e4983d4fa2f60955787518884
souleeater <rimox.midox2011@gmail.com>
2023-08-23 16:16:03 -0400

    l

---
+++ C03/Ex05/ft_strlcat.c
@@ -0,0 +1,25 @@
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = ft_strlen(src) + ft_strlen(dest);
+	if (j > size - 1)
+		return (size);
+	j = ft_strlen(dest);
+	while (src[i] && i < size - 1)
+		dest[j++] = src[i++];
+	dest[j] = '\0';
+	return (ft_strlen(dest));
+}
