ee897e13ef73e66ea99ebc5238e08c272da80a5a
souleeater <rimox.midox2011@gmail.com>
2023-09-03 07:25:37 -0400

    d

--- c08/ex05/ft_show_tab.c
+++ c08/ex05/ft_show_tab.c
@@ -2,32 +2,40 @@
 #include "ft_stock_str.h"
 #include <stdlib.h>
 
-
-void	ft_putstr(char *str)
-{
-	while (*str)
-		write(1, str++, 1);
-}
-
 void	ft_putchar(char c)
 {
 	write(1,&c,1);
 }
 
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+	ft_putchar('\n');
+}
+
 void	ft_putnbr(int nb)
 {
-	long n;
+	long long nbr;
 
-	n = nb;
-	if (n < 0)
+	nbr  = nb;
+	if (nbr < 0)
 	{
-		write(1,"-",1);
-		n = -n;
+		nbr = -nbr;
+		ft_putchar('-');
 	}
-	if (n > 9)
-		ft_putnbr(n/10);
+	if (nbr < 10)
+	{
+		ft_putchar(nbr + '0');
+	}
 	else
-		ft_putchar(n % 10 + '0');
+	{
+		ft_putnbr(nbr / 10);
+		ft_putnbr(nbr % 10);
+	}
 }
 
 void	ft_show_tab(struct s_stock_str *par)
@@ -38,14 +46,9 @@ void	ft_show_tab(struct s_stock_str *par)
 	while (par[i].str)
 	{
 		ft_putstr(par[i].str);
-		ft_putchar('\n');
 		ft_putnbr(par[i].size);
 		ft_putchar('\n');
 		ft_putstr(par[i].copy);
-		ft_putchar('\n');
 		i++;
 	}
-	while (i >= 0)
-		free(par[i--].copy);
-	free(par);
 }
